0ccd8e9c8b56956497a7d3c83af844c496fdc561
Rahul Avale <rahulavale9@gmail.com>
2018-01-05 15:20:11 -0500

    Initial Commit

---
+++ locationAware/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  locationAware
+//
+//  Created by Rahul Avale on 11/30/17.
+//  Copyright © 2017 Rahul Avale. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var manager = CLLocationManager()
+
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        self.latitudeLabel.text = String(location.coordinate.latitude)
+        self.longitudeLabel.text = String(location.coordinate.longitude)
+        self.courseLabel.text = String(location.course)
+        self.speedLabel.text = String(location.speed)
+        self.altitudeLabel.text = String(location.altitude)
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+            if error != nil {
+                print(error)
+            } else {
+                if let placemark = placemarks?[0] {
+                    var address = ""
+                    if placemark.subThoroughfare != nil {
+                        address += placemark.subThoroughfare! + " "
+                    }
+                    if placemark.thoroughfare != nil {
+                        address += placemark.thoroughfare! + "\n"
+                    }
+                    if placemark.subLocality != nil {
+                        address += placemark.subLocality! + "\n"
+                    }
+                    if placemark.subAdministrativeArea != nil{
+                        address += placemark.subAdministrativeArea! + "\n"
+                    }
+                    if placemark.country != nil {
+                        address += placemark.country!
+                    }
+                    self.addressLabel.text = address
+                }
+                    
+            }
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
